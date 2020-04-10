9d4c723366e3d9315d401bd17e073826cfb621d9
lawc7 <63227185+lawc7@users.noreply.github.com>
2020-04-10 16:23:34 +0200

    Add files via upload

---
+++ Components/seoul.m
@@ -0,0 +1,45 @@
+% read data
+
+A = readmatrix('ELISAoverzichtjuni2019_v2.xlsx');
+X = A(:,4);
+X = X(X>0);
+X = log10(X);
+
+hold on
+
+histogram(X,'Binwidth',0.05,'Normalization','pdf','FaceAlpha',0.2,'EdgeAlpha',0.5)
+
+% the model
+
+AIC = zeros(1,4);
+BIC = zeros(1,4);
+negloglik = zeros(1,4);
+GMModels = cell(1,4);
+options = statset('MaxIter',500);
+for k = 1:4
+    GMModels{k} = fitgmdist(X,k,'Options',options);
+    AIC(k) = GMModels{k}.AIC;
+    BIC(k) = GMModels{k}.BIC;
+    negloglik(k) = GMModels{k}.NegativeLogLikelihood;
+end
+
+[minAIC,numComponentsAIC] = min(AIC);
+[minBIC,numComponentsBIC] = min(BIC);
+
+AICModel = GMModels{numComponentsAIC};
+BICModel = GMModels{numComponentsBIC};
+
+fplot(@(x)reshape(pdf(AICModel,x(:)),size(x)),[-3 1],'LineWidth',1)
+fplot(@(x)reshape(pdf(BICModel,x(:)),size(x)),[-3 1],'LineWidth',1)
+% in case numComponentsBIC == 2:
+% fplot(@(x)reshape(pdf(GMModels{2},x(:)),size(x)),[-3 1],'LineWidth',1)
+
+hold off
+
+disp(numComponentsAIC)
+disp(numComponentsBIC)
+
+% comparing goodness-of-fit
+
+prior2on3 = exp(negloglik(2)-negloglik(3));
+disp(prior2on3)
